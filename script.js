4fe43b8a6154e292f52cb146c681b9b3f5aac910
unknown <vtamara.sf@gmail.com>
2023-05-31 23:03:57 -0300

    primeirissimo

---
+++ script.js
@@ -0,0 +1,16 @@
+const botao = document.getElementById('checkbox');
+const corpo = document.getElementById('body');
+const inicio = document.getElementById('inicio');
+const aside = document.getElementById('aside');
+const sobre = document.getElementById('sobremim');
+const projetos = document.getElementById('projetos');
+const competencias = document.getElementById('competencias');
+
+botao.addEventListener('click', () =>{
+    corpo.classList.toggle('dark');
+    inicio.classList.toggle('dark-caixa');
+    aside.classList.toggle('dark-caixa');
+    sobre.classList.toggle('dark-caixa');
+    projetos.classList.toggle('dark-caixa');
+    competencias.classList.toggle('dark-caixa');
+})
