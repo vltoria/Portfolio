0e680ff7f5ea762929d540e6b4934c833fdeb7ed
vltoria <vtamara.sf@gmail.com>
2023-06-01 00:50:29 -0300

    commit 2

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const aside = document.getElementById('aside');
 const sobre = document.getElementById('sobremim');
 const projetos = document.getElementById('projetos');
 const competencias = document.getElementById('competencias');
+const headerPrincipal = document.getElementById('header-principal');
 
 botao.addEventListener('click', () =>{
     corpo.classList.toggle('dark');
@@ -13,4 +14,5 @@ botao.addEventListener('click', () =>{
     sobre.classList.toggle('dark-caixa');
     projetos.classList.toggle('dark-caixa');
     competencias.classList.toggle('dark-caixa');
+    headerPrincipal.classList.toggle('dark');
 })
